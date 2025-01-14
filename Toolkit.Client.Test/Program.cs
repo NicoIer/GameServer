a3a565f09b6d9b1ae224fc119ddcaf404eb4a24e
NicoIer <NicoIer@163.com>
2025-01-14 19:31:32 +0800

    feat:整理基础模块 + 增加对时功能

---
+++ Toolkit.Client.Test/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Network;
+using Network.Client;
+
+IClientSocket socket = new TelepathyClientSocket();
+UriBuilder builder = new UriBuilder
+{
+    Host = "localhost",
+    Port = 8848
+};
+
+NetworkTimeClient client = new NetworkTimeClient(socket);
+socket.Connect(builder.Uri);
+
+Task t1= Task.Run(() =>
+{
+    Console.WriteLine("Press any key to exit...");
+    Console.ReadLine();
+});
+
+Task t2 = Task.Run(() =>
+{
+    while (true)
+    {
+        socket.TickIncoming();
+        socket.TickOutgoing();
+    }
+});
+
+await Task.WhenAny(t1, t2);
---
+++ Toolkit.Server.Test/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+// See https://aka.ms/new-console-template for more information
+
+using Network;
+using Network.Server;
+
+
+TelepathyServerSocket socket = new TelepathyServerSocket
+{
+    port = 8848
+};
+
+TimeSpan pingInterval = TimeSpan.FromSeconds(1);
+TimeSpan pushInterval = TimeSpan.FromSeconds(2);
+NetworkTimeServer timeServer = new NetworkTimeServer(socket, pushInterval, pingInterval);
+
+
+socket.Start();
+
+Task t1= Task.Run(() =>
+{
+    Console.WriteLine("Press any key to exit...");
+    Console.ReadLine();
+});
+
+Task t2 = Task.Run(() =>
+{
+    while (true)
+    {
+        socket.TickIncoming();
+        socket.TickOutgoing();
+    }
+});
+
+await Task.WhenAny(t1, t2);
